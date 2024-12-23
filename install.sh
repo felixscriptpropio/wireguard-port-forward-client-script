b754910df05ee5207fbcf0bfc23046eebedc00c0
felixscriptpropio <carolinapinedaa91@gmail.com>
2024-12-22 23:29:59 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -68,10 +68,6 @@ services:
     ports:
       - "51820:51820/udp"
       - "51821:51821/tcp"
-      - "5500:5500/tcp"
-      - "5050:5050/tcp"
-      - "5051:5051/tcp"
-      - "5052:5052/tcp"
       - "5053:5053/tcp"
       - "5054:5054/tcp"
       - "5055:5055/tcp"
@@ -79,9 +75,8 @@ services:
       - "7777:7777/tcp"
       - "8888:8888/tcp"
       - "9090:9090/tcp"
-      - "9999:9999/tcp"
       - "800:800/tcp"
-      - "8000:8000/tcp"
+      - "9999:9999/tcp"
 EOF
 
 # Restrict permissions
@@ -103,7 +98,7 @@ EXT_IFACE="eth0"
 VPN_CLIENT_IP="10.8.0.2"
 
 # List of ports to forward
-PORTS=(5050 5051 5052 5053 5054 5055 6666 7777 8888 9090 9999 800 8000 5500)
+PORTS=(5053 5054 5055 6666 7777 8888 9090 9999 800)
 
 # Loop through each port and add iptables rules inside the container
 for PORT in "${PORTS[@]}"; do
